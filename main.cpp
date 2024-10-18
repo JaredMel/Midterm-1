c705ff18d3da3095c6a95935998f325f6cda3bdb
JaredMel <jareddmelendez1764@gmail.com>
2024-10-17 19:18:53 -0700

    WIP

--- main.cpp
+++ main.cpp
@@ -77,56 +77,70 @@ public:
         if (!temp) return;
         //Checks if value is at the head
         if (temp->prev)
+            //temp points to the prev points to the next and sets it to temp pointing to next
             temp->prev->next = temp->next;
         else
+            //sets the head equal to the next node
             head = temp->next;
-
+        //Checks if value is at the tail
         if (temp->next)
+            //temp points to the next points to the prev and sets it to temp pointing to prev
             temp->next->prev = temp->prev;
         else
+            //sets the tail to the prev node
             tail = temp->prev;
-
+        //deletes temp Node
         delete temp;
     }
-
+    //the delete_pos method
     void delete_pos(int pos) {
+        //Checks if list is empty
         if (!head) {
             cout << "List is empty." << endl;
             return;
         }
-
+        //checks if position is at the head
         if (pos == 1) {
+            //calls the pop_front() method
             pop_front();
             return;
         }
-
+        //Creates a temp node and sets it to head
         Node* temp = head;
-
+        //for loop that iterates in till i = pos
         for (int i = 1; i < pos; i++){
+            //checks if it reached the end of the list
             if (!temp) {
                 cout << "Position doesn't exist." << endl;
                 return;
             }
             else
+                //iterates to the next node
                 temp = temp->next;
         }
+        //checks if it reached the end of the list
         if (!temp) {
             cout << "Position doesn't exist." << endl;
             return;
         }
-
+        //checks if position is at the tail
         if (!temp->next) {
+            //calls the pop_back() method
             pop_back();
             return;
         }
-
+        //creates a tempPrev node and points it to the prev node behind temp
         Node* tempPrev = temp->prev;
+        //Sets the next pointer for tempPrev to the next pointer for temp
         tempPrev->next = temp->next;
+        //Sets the next pointer pointing to the prev for temp to the prev pointer for tempPrev
         temp->next->prev = tempPrev;
+        //deletes the temp node
         delete temp;
     }
-
+    //the push_back method
     void push_back(int v) {
+        //
         Node* newNode = new Node(v);
         if (!tail)
             head = tail = newNode;
